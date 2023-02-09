4fbb1b39b5ed1d8f1c5072a7bf1b58872bbe8aec
praveen-sbr <praveensbr2018@gmail.com>
2023-02-09 13:31:27 +0530

    initial commit

---
+++ src/main/java/com/spring/user/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.spring.user.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+	
+	@GetMapping("status")
+	public ResponseEntity<String> getStatus()
+	{
+		return new ResponseEntity<String>("UserService Running",HttpStatus.OK);
+	}
+
+	
+}
